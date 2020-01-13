0fe5e73a29f8f3e3bebf4ef8d8ddbc3a569e5c50
oldjamey <oldjamey@icloud.com>
2020-01-13 16:23:57 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Caesar Cypher
+This is code that I modified from an example in a book to make it more useful.
+
+
+Usage:
+./caesar.py [NUMBER OF SHIFTS] [-e|-d]
---
+++ caesar.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+####################################################################
+# Copyright (C) 2020 phx <https://github.com/phx>
+# This is an updated version of the original Caesar Cipher code from
+# https://www.nostarch.com/crackingcodes (BSD Licensed)
+# All code changes made by phx.
+# This is currently under development to be cleaned up.
+####################################################################
+import sys
+
+# The encryption/decryption key:
+if len(sys.argv) < 1:
+    key = 0
+    print('shift:' + str(key) + '\n')
+    mode = 'decrypt'
+else:
+    key = int(sys.argv[1])
+    print('shift: ' + str(key) + '\n')
+
+# Whether the program encrypts or decrypts:
+if len(sys.argv) > 2:
+    if '-d' in sys.argv[2]:
+        mode = 'decrypt'
+    elif '-e' in sys.argv[2]:
+        mode = 'encrypt'
+    else:
+        print("Must include either '-e' or '-d' flags")
+        raise SystemExit
+else:
+    key = int(sys.argv[1])
+    mode = 'decrypt'
+
+# Every possible symbol that can be encrypted:
+SPECIAL = '.,;:\'"<>[]{}|\/+-=()*&^%$#@!?`~_ 	\n'
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+# The string to be encrypted/decrypted:
+# input = [x.replace('\n', ' ') for x in sys.stdin.readlines()]
+input = [x for x in sys.stdin.readlines()]
+output = []
+
+# Stores the encrypted/decrypted form of the message:
+translated = ''
+
+for i, _ in enumerate(input):
+    message = list(input[i])
+    for symbol in message:
+        symbol = symbol.upper()
+        # Note: Only symbols in the `SYMBOLS` string can be encrypted/decrypted.
+        if SPECIAL.find(symbol) == True:
+            output.append(symbol)
+        elif symbol in SYMBOLS:
+            # Perform encryption/decryption:
+            symbolIndex = SYMBOLS.find(symbol)
+            if mode == 'encrypt':
+                translatedIndex = symbolIndex + key
+            elif mode == 'decrypt':
+                translatedIndex = symbolIndex - key
+
+            # Handle wrap-around, if needed:
+            if translatedIndex >= len(SYMBOLS):
+                translatedIndex = translatedIndex - len(SYMBOLS)
+            elif translatedIndex < 0:
+                translatedIndex = translatedIndex + len(SYMBOLS)
+
+            output.append(translated)
+            translated = translated + SYMBOLS[translatedIndex]
+        else:
+            # Append the symbol without encrypting/decrypting:
+            translated = translated + symbol
+            output.append(translated)
+
+# Output the translated string:
+print(output[len(output)-1])
